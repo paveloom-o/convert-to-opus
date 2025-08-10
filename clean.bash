0c5050f714ef1ddab95c8f14563a3d7697c79615
Pavel Sobolev <contact@paveloom.dev>
2025-08-10 10:20:15 +0300

    Avoid deleting the `.git` directory.

--- clean.bash
+++ clean.bash
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 root="$(dirname "$(realpath -s "$0")")"
-find "$root" -mindepth 1 -type d -exec rm -rf {} +
+find "$root" -mindepth 1 -maxdepth 1 -type d -not -name .git -exec rm -rf {} +
